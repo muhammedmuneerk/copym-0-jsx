92e5a16f09ec6e0a470217ea68aa4c5cf0eed83b
muhammedmuneerk <muhammedmuneerk15@gmail.com>
2025-04-07 00:21:07 +0530

    copym navbar logo increased to  md w40 h18

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,7 +92,7 @@ export default function Navbar() {
               <img
                 src="/assets/icons/logo-svg.svg"
                 alt="COPYM"
-                className="w-20 h-10 sm:w-30 sm:h-15 md:w-36 md:h-16 lg:w-48 lg:h-20 xl:w-50 xl:h-20"
+                className="w-20 h-10 sm:w-30 sm:h-15 md:w-40 md:h-18 lg:w-48 lg:h-20 xl:w-50 xl:h-20"
               />
             </Typography>
           </motion.div>
