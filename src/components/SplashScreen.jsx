af2a5d02934d6417d40abbfda3fc9482a824c87c
muhammedmuneerk <muhammedmuneerk15@gmail.com>
2025-04-08 23:57:58 +0530

    fix starting contents positions on mobile 3

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -53,7 +53,7 @@ const SplashScreen = () => {
 
     // Adjust the vertical shift amount based on device size
     const verticalShift = isMobile
-        ? "-mt-14" // More shift on mobile
+        ? "-mt-18" // More shift on mobile
         : isTablet
             ? "-mt-12" // Medium shift on tablet
             : "-mt-8"; // Small shift on desktop
